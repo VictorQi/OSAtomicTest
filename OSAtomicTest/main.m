c59aa0f1f2121523b60a658877993db888c7d590
VictorQi <hust_yjc@163.com>
2017-04-18 10:31:53 +0800

    Initial Commit

---
+++ OSAtomicTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OSAtomicTest
+//
+//  Created by Victor on 2017/4/18.
+//  Copyright © 2017年 VictorQi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
